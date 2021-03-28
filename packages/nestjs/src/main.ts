51d8f53e98bb1558db8b325bbbac861a0ee2f787
Arnaud Cortisse <arnaud@koopol.com>
2021-03-28 15:57:35 +0200

    done

--- packages/nestjs/src/main.ts
+++ packages/nestjs/src/main.ts
@@ -5,7 +5,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: 'http://localhost:3000',
+  });
 
   const config = new DocumentBuilder()
     .setTitle('Tasks')
